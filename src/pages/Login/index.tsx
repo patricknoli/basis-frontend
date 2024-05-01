0b6e1e55f37c88055dcf2bfe28a43228ab8d28a7
Patrick Oliveira <patrick@torors.com.br>
2024-05-01 14:52:44 -0300

    feat: add login api

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,36 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { i18n } from "../../i18n"
 import { AppContext } from "../../contexts/AppContext"
 import Header from "../../components/Header/External";
 import { Link } from "react-router-dom";
-import { Button, TextField } from "@mui/material";
-import { BiChevronRight } from "react-icons/bi";
+import { InputAdornment, TextField } from "@mui/material";
+import { BiChevronRight, BiLock, BiUser } from "react-icons/bi";
+import { FieldValues, useForm } from "react-hook-form";
+import { withMask } from "use-mask-input";
+import { LoadingButton } from "@mui/lab";
+import { api } from "../../services/api";
 
 const Login: React.FC = () => {
   const { lang } = useContext(AppContext);
+  const { register, handleSubmit } = useForm();
+  const [ isSubmitting, setIsSubmitting ] = useState<boolean>(false);
+
+  async function loginUser(fields: FieldValues) {
+    setIsSubmitting(true);
+    try {
+      const params = {
+        login: fields.login,
+        senha: fields.password,
+        idBanco: ""
+      }
+      let { data } = await api.get('/login/usuario', {params});
+      console.log(data);
+      setIsSubmitting(false);
+    } catch(error) {
+      console.log(error);
+      setIsSubmitting(false);
+    }
+  }
 
   return (
     <>
@@ -16,17 +39,36 @@ const Login: React.FC = () => {
         <h1 className="text-3xl font-bold">{i18n[lang].login_title}</h1>
         <p className="mt-3">{i18n[lang].login_subtitle}</p>
 
-        <form className="flex flex-col gap-3 my-6">
-          <TextField label={i18n[lang].login_input_document} variant="outlined" />
-          <TextField label={i18n[lang].login_input_password} variant="outlined" />
+        <form onSubmit={handleSubmit(loginUser)} className="flex flex-col gap-3 my-6">
+          <TextField label={i18n[lang].login_input_document} variant="outlined"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <BiUser />
+              </InputAdornment>
+            ),
+          }}
+          inputProps={<input ref={withMask('cpf')} />}
+          {...register('login')} />
+          <TextField type="password" {...register('password')} 
+          label={i18n[lang].login_input_password} variant="outlined"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <BiLock />
+              </InputAdornment>
+            ),
+          }}
+          />
 
           <Link className="text-sm font-semibold mb-6" to="/login/reset">
             {i18n[lang].login_forgot_password}
           </Link>
 
-          <Button variant="contained" endIcon={<BiChevronRight />}> 
+          <LoadingButton variant="contained" endIcon={<BiChevronRight />}
+          loading={isSubmitting} type="submit"> 
             Login
-          </Button>
+          </LoadingButton>
         </form>
 
         <hr />
