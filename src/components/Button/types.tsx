8d0f88621ed16a22a69c7da7eaac18b985125eeb
Patrick Oliveira <patrick@torors.com.br>
2024-05-01 13:50:02 -0300

    feat: add MUI lib and update login components

--- src/components/Button/types.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react"
-
-export type ButtonProps = {
-  children: ReactNode;
-  icon?: ReactNode;
-  width: "full" | "auto";
-}
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,9 +2,9 @@ import { useContext } from "react"
 import { i18n } from "../../i18n"
 import { AppContext } from "../../contexts/AppContext"
 import Header from "../../components/Header/External";
-import Button from "../../components/Button";
-import { BiChevronRight } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import { Button, TextField } from "@mui/material";
+import { BiChevronRight } from "react-icons/bi";
 
 const Login: React.FC = () => {
   const { lang } = useContext(AppContext);
@@ -17,14 +17,14 @@ const Login: React.FC = () => {
         <p className="mt-3">{i18n[lang].login_subtitle}</p>
 
         <form className="flex flex-col gap-3 my-6">
-          <input type="text" placeholder={i18n[lang].login_input_document} />
-          <input type="text" placeholder={i18n[lang].login_input_password} />
+          <TextField label={i18n[lang].login_input_document} variant="outlined" />
+          <TextField label={i18n[lang].login_input_password} variant="outlined" />
 
           <Link className="text-sm font-semibold mb-6" to="/login/reset">
             {i18n[lang].login_forgot_password}
           </Link>
 
-          <Button width="full" icon={<BiChevronRight />}> 
+          <Button variant="contained" endIcon={<BiChevronRight />}> 
             Login
           </Button>
         </form>
