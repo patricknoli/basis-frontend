3e94b4287717a1602a59123aac32251672311ed4
Patrick Oliveira <patrick@torors.com.br>
2024-05-03 14:31:59 -0300

    feat: adjust desktop version for login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -9,6 +9,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import { withMask } from "use-mask-input";
 import { LoadingButton } from "@mui/lab";
 import { api } from "../../services/api";
+import hero from "../../assets/mock-hero.png";
 
 const Login: React.FC = () => {
   const { lang, updateUser } = useContext(AppContext);
@@ -46,50 +47,59 @@ const Login: React.FC = () => {
 
   return (
     <>
-      <Header />
-      <div className="p-5">
-        <h1 className="text-3xl font-bold">{i18n[lang].login_title}</h1>
-        <p className="mt-3">{i18n[lang].login_subtitle}</p>
+      <div className="md:hidden">
+        <Header />
+      </div>
+      <div className="flex h-[100vh]">
+        <div className="hero hidden md:block basis-[45%]">
+          <img src={hero} alt="" className="w-full h-[100vh]" />
+        </div>
+        <div className="p-5 flex items-start md:items-center justify-center basis-full md:basis-[55%]">
+          <div className="max-w-[500px] w-full">
+            <h1 className="text-3xl font-bold">{i18n[lang].login_title}</h1>
+            <p className="mt-3">{i18n[lang].login_subtitle}</p>
 
-        <form onSubmit={handleSubmit(loginUser)} className="flex flex-col gap-3 my-6">
-          <TextField label={i18n[lang].login_input_document} variant="outlined"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <BiUser />
-              </InputAdornment>
-            ),
-          }}
-          inputProps={<input ref={withMask('cpf')} />}
-          {...register('login')} />
-          <TextField type="password" {...register('password')} 
-          label={i18n[lang].login_input_password} variant="outlined"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <BiLock />
-              </InputAdornment>
-            ),
-          }}
-          />
+            <form onSubmit={handleSubmit(loginUser)} className="flex flex-col gap-3 my-6">
+              <TextField label={i18n[lang].login_input_document} variant="outlined"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <BiUser />
+                  </InputAdornment>
+                ),
+              }}
+              inputProps={<input ref={withMask('cpf')} />}
+              {...register('login')} />
+              <TextField type="password" {...register('password')} 
+              label={i18n[lang].login_input_password} variant="outlined"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <BiLock />
+                  </InputAdornment>
+                ),
+              }}
+              />
 
-          <Link className="text-sm font-semibold mb-6" to="/login/reset">
-            {i18n[lang].login_forgot_password}
-          </Link>
+              <Link className="text-sm font-semibold mb-6" to="/login/reset">
+                {i18n[lang].login_forgot_password}
+              </Link>
 
-          <LoadingButton variant="contained" endIcon={<BiChevronRight />}
-          loading={isSubmitting} type="submit"> 
-            Login
-          </LoadingButton>
-        </form>
+              <LoadingButton variant="contained" endIcon={<BiChevronRight />}
+              loading={isSubmitting} type="submit"> 
+                Login
+              </LoadingButton>
+            </form>
 
-        <hr />
+            <hr />
 
-        <div className="flex items-center justify-between mt-6">
-          <span className="text-sm text-zinc-500 font-semibold">{i18n[lang].login_create_account_cta}</span>
-          <Link className="text-sm font-semibold" to="/register">
-            {i18n[lang].login_create_account_link}
-          </Link>
+            <div className="flex items-center justify-between mt-6">
+              <span className="text-sm text-zinc-500 font-semibold">{i18n[lang].login_create_account_cta}</span>
+              <Link className="text-sm font-semibold" to="/register">
+                {i18n[lang].login_create_account_link}
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
 
