5c19174fc844ca68100cd0ee88f4bf7879edba2d
Patrick Oliveira <patrick@torors.com.br>
2024-05-06 16:55:50 -0300

    chore: update login component testing

--- src/pages/Login/indexLogin.cy.tsx
+++ src/pages/Login/indexLogin.cy.tsx
@@ -1,9 +1,26 @@
+import { MemoryRouter, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Login from './index'
 import { mount } from "cypress/react18"
+import { AppProvider } from '../../contexts/AppContext'
 
 describe('<Login />', () => {
-  it('renders', () => {
+  it('validates input document', () => {
     // see: https://on.cypress.io/mounting-react
-    mount(<Login />)
+    mount(
+      <MemoryRouter initialEntries={
+        ['/login']
+      }>
+        <AppProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </AppProvider>
+      </MemoryRouter>
+    )
+    cy.get('input[name="login"').type("00").blur();
+    cy.get('p.MuiFormHelperText-root').should('not.be.empty');
+    cy.get('input[name="login"]').clear().type("92981451014").should('have.value', '929.814.510-14').blur();
+    cy.get('button[type="submit"]').click();
+    cy.get('div.MuiSnackbarContent-message').should('not.be.empty');
   })
 })
